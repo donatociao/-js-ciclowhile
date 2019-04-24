3bdad259159f745e840de471e01f1a9dfe5bd329
donatociao <ciao.donatociao@gmail.com>
2019-04-24 10:30:46 +0200

    Ok controlli

--- main.js
+++ main.js
@@ -4,18 +4,45 @@ var elenco = ["ancona", "bali", "cervo", "dati", "elano", "fabbri"];
 //creo un prompt per la richiesta del cognome
 var surname = prompt ("Inserisci il tuo cognome");
 
-//inserisco il cognome nell'array
-elenco.push(surname);
+//dichiaro index
+var i = 0;
 
-//ordino i cognomi
-elenco.sort()
+//dichiaro variabile booleana
+var insert = false;
+
+//controllo se il cognome è già presente in elenco
+while (i < elenco.length){
+
+  //se presente cambio variabile booleana
+  if (elenco[i] === surname) {
+    insert = true;
+  }
+  i++;
+}
+
+//se il cognome non è già presente in lista
+if (insert == false) {
+
+  //dico che il cognome è stato inserito in lista
+  document.writeln("Il cognome è stato inserito nella lista.</br>")
+
+  //inserisco il cognome nell'array
+  elenco.push(surname);
+
+  //ordino i cognomi
+  elenco.sort()
+} else {
+
+  //se il cognome è già presente in lista
+  document.writeln("Il cognome è già presente nella lista.</br>")
+}
 
 //dichiaro variabile posizione
 var posizione = (elenco.indexOf(surname) + 1);
 
 document.writeln("I cognomi in lista sono:" );
 
-//dichiaro e inizializzo indice
+//dichiaro index
 var j = 0;
 
 while (j < elenco.length){
@@ -28,4 +55,4 @@ while (j < elenco.length){
 }
 
 //stampo la posizione del nuovo cognome inserito
-document.writeln("</br>Il tuo cognome è in posizione " + posizione + ".");
+document.writeln("</br>Il cognome " + surname + " è in posizione " + posizione + ".");
